469c1c6f87355c041f6f59aefe2d91fc78a28602
FelixVardanyan <felixvardanyan2005@gmail.com>
2025-09-26 16:08:37 +0400

    redux2 + about update

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
+import { aboutReducer } from "./about/reducer";
 const rootReducer = combineReducers({
-
+    text: aboutReducer
 })
 
 export default rootReducer
